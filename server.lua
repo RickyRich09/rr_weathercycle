ecc793582895df73805c2f67bb037cf7bcd87350
RickyRich09 <rudradipsarkar9@gmail.com>
2025-02-05 00:07:46 +0530

    Enhance weather change logic to cycle through types based on current weather

--- server.lua
+++ server.lua
@@ -4,23 +4,43 @@ local ChangeInterval = WeatherShared.Configuration.ChangeTime * 60 * 1000
 local DebugChangeInterval = WeatherShared.Configuration.DebugChangeTime * 1000
 
 local function ChangeWeather()
-    local newWeather = WeatherShared.Configuration.Types[math.random(#WeatherShared.Configuration.Types)]
+    local currentWeather = exports['qb-weathersync']:GetWeather() -- GET CURRENT WEATHER
+    local weatherTypes = WeatherShared.Configuration.Types
+    local newWeather
+
+    -- FIND THE INDEX OF THE CURRENT WEATHER
+    local currentIndex = nil
+    for i, weather in ipairs(weatherTypes) do
+        if weather == currentWeather then
+            currentIndex = i
+            break
+        end
+    end
+
+    -- DETERMINE THE NEXT WEATHER TYPE IN ORDER
+    if currentIndex and currentIndex < #weatherTypes then
+        newWeather = weatherTypes[currentIndex + 1]
+    else
+        newWeather = weatherTypes[1] -- LOOP BACK TO THE BEGINNING
+    end
+
+    -- SET THE NEW WEATHER
     TriggerEvent('qb-weathersync:server:setWeather', newWeather)
 
-    if WeatherShared.Configuration.Debug == true then
-        print("Weather Changed To: " .. newWeather)
+    if WeatherShared.Configuration.Debug == true then --- DEBUG PRINT
+        print("[DEBUG] Weather Changed To: " .. newWeather)
     elseif WeatherShared.Configuration.Debug == false then
         return
     end
 end
 
--- Start the weather cycle
+-- START THE WEATHER CYCLE
 Citizen.CreateThread(function()
     while true do
         ChangeWeather()
-        if WeatherShared.Configuration.Debug == true then
+        if WeatherShared.Configuration.Debug == true then -- DEBUG TIME
             Citizen.Wait(DebugChangeInterval)
-        elseif WeatherShared.Configuration.Debug == false then
+        elseif WeatherShared.Configuration.Debug == false then -- NORMAL TIME
             Citizen.Wait(ChangeInterval)
         end
     end
